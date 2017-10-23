a28f957fdb7f40f09afc3318b157795563cfaa84
uaman@gmail.com <git100500>
2017-10-24 01:06:17 +0300

    feat: add UserService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {UsersService} from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Img:Uploader';
+
+  constructor(us: UsersService) {
+    console.log(`users:`, us.users);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { UploadComponent } from './components/upload/upload.component';
-import { TableViewComponent } from './components/table-view/table-view.component';
-import { BlockViewComponent } from './components/block-view/block-view.component';
+
+import {AppComponent} from './app.component';
+import {LoginComponent} from './components/login/login.component';
+import {UploadComponent} from './components/upload/upload.component';
+import {TableViewComponent} from './components/table-view/table-view.component';
+import {BlockViewComponent} from './components/block-view/block-view.component';
 import {AppRoutingModule} from './app.routing.module';
+import {UsersService} from './services/users.service';
 
 @NgModule({
   declarations: [
@@ -21,10 +23,13 @@ import {AppRoutingModule} from './app.routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    UsersService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
