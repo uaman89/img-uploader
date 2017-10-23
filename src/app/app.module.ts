5a5f671f24db901a87eb999734e744d003ca0ff2
uaman@gmail.com <git100500>
2017-10-23 23:34:56 +0300

    feat: bootstrap project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { TableViewComponent } from './components/table-view/table-view.component';
+import { BlockViewComponent } from './components/block-view/block-view.component';
+import {AppRoutingModule} from './app.routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UploadComponent,
+    TableViewComponent,
+    BlockViewComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,32 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {UploadComponent} from './components/upload/upload.component';
+import {TableViewComponent} from './components/table-view/table-view.component';
+import {BlockViewComponent} from './components/block-view/block-view.component';
+
+
+export const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'upload', component: UploadComponent},
+  {
+    path: 'view',
+    children: [
+      {path: 'table', component: TableViewComponent},
+      {path: 'block', component: BlockViewComponent},
+    ]
+  },
+  // {path: '**', redirectTo: '/'}
+];
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {
+}
