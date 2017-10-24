6b6a92eacf4c0df9062d68e2e44abd90ac10f54b
Olexandr P <polischuk@cinegy.com>
2017-10-24 18:22:50 +0300

    feat: add AuthService [WIP]

---
+++ src/app/services/auth-service.service.ts
@@ -0,0 +1,41 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+interface UserCredentials {
+  email: string;
+  password: string;
+}
+
+@Injectable()
+export class AuthServiceService {
+
+  private users: any[];
+
+  constructor(private http: HttpClient) {
+
+    http.get('/assets/users.json').subscribe(
+      res => {
+        this.users = res['users'];
+      },
+      error => {
+        console.error('at auth_service:', error);
+      }
+    );
+
+  }
+
+  private getUsers() {
+    if (this.users !== undefined) {
+      return this.users;
+    }
+  }
+
+  public login(user: UserCredentials) {
+
+  }
+
+  public logout() {
+
+  }
+
+}
