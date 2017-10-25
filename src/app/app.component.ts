a4b4b23e88d1bc8a82b1486200ad30ac540a8b2a
Olexandr P <polischuk@cinegy.com>
2017-10-25 12:31:32 +0300

    feat(app): add default redirects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {AuthService} from './services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,11 @@ import {AuthService} from './services/auth.service';
 export class AppComponent {
   title = 'Img:Uploader';
 
-  constructor(auth: AuthService) {
+  constructor(auth: AuthService, router: Router) {
+    if (!auth.isAuthorized){
+      router.navigate(['login']);
+    } else {
+      router.navigate(['view/block']);
+    }
   }
 }
