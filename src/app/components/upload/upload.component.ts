2869b2735905f95b81159c484ed450e13c8844af
uaman@gmail.com <git100500>
2017-10-26 02:38:29 +0300

    feat(upload component): WIP: implement save()

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,4 +1,14 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
+
+interface IImageModel {
+  'id': number;
+  'imageName': string;
+  'fileSize': string; // example 114.6 kB
+  'checksum': string; // SHA256
+  'uploadedUser': number; // user id
+}
 
 @Component({
   selector: 'app-upload',
@@ -6,9 +16,9 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent implements OnInit {
-  public previews: Set<File> = new Set([]);
+  public previews: Set<IImageModel> = new Set([]);
 
-  constructor() {
+  constructor(private auth: AuthService, private router: Router) {
   }
 
   ngOnInit() {
@@ -16,18 +26,34 @@ export class UploadComponent implements OnInit {
 
   public updatePreviews(files) {
     // todo: detect duplicates
-    files.map(f => this.previews.add(f));
+    files.map((f: File) => this.previews.add({
+      id: Date.now(),
+      imageName: f.name,
+      fileSize: `${f.size / 1000} kB`,
+      checksum: `getChecksum()`,
+      uploadedUser: this.auth.user.id
+    }));
   }
 
   public removePreview(preview) {
     this.previews.delete(preview);
   }
 
-  public clearAll(){
+  public clearAll() {
     this.previews.clear();
   }
 
   save() {
-
+    if (!this.auth.isAuthorized) {
+      this.router.navigate(['/login']);
+      return;
+    }
+    let uploadedImages: IImageModel[] = JSON.parse(localStorage.getItem('uploadedImages'));
+    if (!(uploadedImages instanceof Array)) {
+      uploadedImages = [];
+    }
+    this.previews.forEach(value => uploadedImages.push(value));
+    localStorage.setItem('uploadedImages', JSON.stringify(uploadedImages));
+    this.clearAll();
   }
 }
