e7bf93ef796da8406627652f2e15b285c733b670
Olexandr P <polischuk@cinegy.com>
2017-10-25 13:53:30 +0300

    feat(login component): add some validation, update markup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserCredentials} from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public user: UserCredentials = {
+    email: '',
+    password: ''
+  };
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
