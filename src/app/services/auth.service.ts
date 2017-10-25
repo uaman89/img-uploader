e31373cbe25d07b4899511a69a72a8b561e97e6b
uaman@gmail.com <git100500>
2017-10-26 01:52:20 +0300

    refactor(auth service): save auth-ed user ID

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,18 +12,18 @@ export class AuthService {
   public isAuthorized = false;
 
   constructor(private users: UsersService) {
-    this.isAuthorized = localStorage.getItem('isAuthorized') === 'true';
+    this.isAuthorized = localStorage.getItem('authorizedUser') === 'true';
   }
 
-  public login(user: IUserCredentials) {
+  public login(credentials: IUserCredentials) {
 
     return this.users.getUsers().then(users => {
 
-      const isUserExists = !!users.find(u => u.email === user.email && u.password === user.password);
+      const user = users.find(u => u.email === credentials.email && u.password === credentials.password);
 
-      if (isUserExists) {
+      if (!!user) {
         this.isAuthorized = true;
-        localStorage.setItem('isAuthorized', this.isAuthorized.toString());
+        localStorage.setItem('authorizedUser', user.id);
       } else {
         this.logout();
       }
@@ -35,7 +35,7 @@ export class AuthService {
 
   public logout() {
     this.isAuthorized = false;
-    localStorage.removeItem('isAuthorized');
+    localStorage.removeItem('authorizedUser');
   }
 
 }
