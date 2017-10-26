23612c0318987c5c6c46367d3fbec97c9a5ddc75
uaman@gmail.com <git100500>
2017-10-27 02:51:32 +0300

    chore(block-view): unsubscribe onDestroy

--- src/app/components/block-view/block-view.component.ts
+++ src/app/components/block-view/block-view.component.ts
@@ -1,22 +1,24 @@
-import {Component, OnInit} from '@angular/core';
-import {IImageModel, ImageStoreService} from '../../services/image-store.service';
-import {UsersService} from '../../services/users.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {IImageModel, ImageStoreService} from 'app/services/image-store.service';
+import {UsersService} from 'app/services/users.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-block-view',
   templateUrl: './block-view.component.html',
   styles: []
 })
-export class BlockViewComponent implements OnInit {
+export class BlockViewComponent implements OnInit, OnDestroy {
 
   public images: IImageModel[] = [];
+  private subscription: Subscription;
 
   constructor(public imageStore: ImageStoreService, private userService: UsersService) {
   }
 
   ngOnInit() {
 
-    this.imageStore.list.subscribe(storedImages => {
+    this.subscription = this.imageStore.list.subscribe(storedImages => {
 
       this.images = storedImages.map(img => {
         this.userService.getUsers().then(list => {
@@ -31,6 +33,10 @@ export class BlockViewComponent implements OnInit {
 
   delImage(index) {
     this.imageStore.deleteImage(this.images[index].checksum);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
