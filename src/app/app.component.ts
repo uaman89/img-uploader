19253454ec88f61369dd558400bf0e31cf458cd6
uaman@gmail.com <git100500>
2017-10-27 02:59:37 +0300

    feat: clear Images on logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {AuthService} from './services/auth.service';
 import {NavigationEnd, Router, Event} from '@angular/router';
 import {PATH_BLOCKS, PATH_LOGIN} from './constants';
+import {ImageStoreService} from './services/image-store.service';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,7 @@ import {PATH_BLOCKS, PATH_LOGIN} from './constants';
 export class AppComponent {
   title = 'Img:Uploader';
 
-  constructor(public auth: AuthService, private router: Router) {
+  constructor(public auth: AuthService, private router: Router, private imageStore: ImageStoreService) {
     if (!auth.isAuthorized) {
       router.navigate([PATH_LOGIN]);
     } else {
@@ -29,6 +30,7 @@ export class AppComponent {
   public logout(e) {
     e.preventDefault();
     this.auth.logout();
+    this.imageStore.clear();
     this.router.navigate([PATH_LOGIN]);
   }
 }
