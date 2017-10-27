0fc0557a9aa228976e2af7de0e09df1d26293cc0
uaman@gmail.com <git100500>
2017-10-27 03:44:06 +0300

    refactor(block-view): make it use view-service

--- src/app/components/block-view/block-view.component.ts
+++ src/app/components/block-view/block-view.component.ts
@@ -1,42 +1,14 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {IImageModel, ImageStoreService} from 'app/services/image-store.service';
-import {UsersService} from 'app/services/users.service';
-import {Subscription} from 'rxjs/Subscription';
+import {Component} from '@angular/core';
+import {ViewService} from '../../services/view.service';
 
 @Component({
   selector: 'app-block-view',
   templateUrl: './block-view.component.html',
-  styles: []
+  providers: [ViewService]
 })
-export class BlockViewComponent implements OnInit, OnDestroy {
+export class BlockViewComponent {
 
-  public images: IImageModel[] = [];
-  private subscription: Subscription;
-
-  constructor(public imageStore: ImageStoreService, private userService: UsersService) {
-  }
-
-  ngOnInit() {
-
-    this.subscription = this.imageStore.list.subscribe(storedImages => {
-
-      this.images = storedImages.map(img => {
-        this.userService.getUsers().then(list => {
-          img['uploadUserName'] = (list.find(u => u.id === img.uploadedUser)).name;
-        });
-        return img;
-      });
-
-    });
-
-  }
-
-  delImage(index) {
-    this.imageStore.deleteImage(this.images[index].checksum);
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+  constructor(public view: ViewService) {
   }
 
 }
